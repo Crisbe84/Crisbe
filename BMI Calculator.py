863c5031406eb031f535abbecf436669060f59dd
Crisbe84 <54870090+Crisbe84@users.noreply.github.com>
2025-02-09 21:33:14 -0500

    Add files via upload
    Playing with numbers

---
+++ BMI Calculator.py
@@ -0,0 +1,10 @@
+print("BMI Calculator\n\nWanna figure out your total BMI?\nThis is the calculator for you! Please answer the following questions for me to calculate your total Body Mass Index.\n")
+
+height = float(input("What is your height in inches?\n"))
+weight = float(input("Now the hard part, I won't look. What is your weight in pounds?\n"))
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = (weight*703)/height/height
+
+print("Your total BMI is: ",int(bmi))
