9c0a5f7ff896d8b1ea2a0eeb90e25acf91021d02
Crisbe84 <54870090+Crisbe84@users.noreply.github.com>
2025-02-17 14:19:33 -0500

    Update BMI Calculator.py
    Added BMI catergory to code

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -3,8 +3,16 @@ print("BMI Calculator\n\nWanna figure out your total BMI?\nThis is the calculato
 height = float(input("What is your height in inches?\n"))
 weight = float(input("Now the hard part, I won't look. What is your weight in pounds?\n"))
 
-# Write your code here.
-# Calculate the bmi using weight and height.
-bmi = (weight*703)/height/height
+# Calculate BMI
+bmi = (weight * 703) / (height ** 2)
 
-print("Your total BMI is: ",round(bmi, 2))
+# Display result
+print(f"Your total BMI is: {round(bmi, 2)}")
+
+# Determine BMI category
+if bmi < 18.5:
+    print("You are Underweight.")
+elif 18.5 <= bmi <= 24.9:
+    print("You have a Normal weight.")
+else:
+    print("You are Overweight.")
