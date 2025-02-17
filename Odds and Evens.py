bc411e4ed98ce8a31f8ebd7a242add0ec40bb6b2
Crisbe84 <54870090+Crisbe84@users.noreply.github.com>
2025-02-17 12:47:37 -0500

    Modulo Testing

---
+++ Odds and Evens.py
@@ -0,0 +1,11 @@
+# Even and Odds
+print("Welcome to Odds and Evens, please enter a number.\n")
+
+# Input
+num = int(input("Please select a number:\n"))
+
+# If/Else
+if num % 2 == 0:
+    print("\nThis number is even. What else do ya wanna know?")
+else:
+    print("\nThis number is odd, and so is this question.")
